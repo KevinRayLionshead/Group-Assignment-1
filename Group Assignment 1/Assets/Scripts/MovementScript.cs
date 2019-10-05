c7cea8a46d04235da94334780e1fdbbea6470d6d
Dimitris <dimitrios.stefanakos@ontariotechu.net>
2019-10-04 22:13:27 -0400

    Changed player character and added in movement for them with WASD

---
+++ Group Assignment 1/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class MovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A)) //Press A to move left
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D)) //Press D to move right
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.W)) //Press W to move forward
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S)) //Press S to move backwards
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        /*
+        if (Input.GetKeyDown(KeyCode.Space)) //Press space to jump
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        */
+    }
+}
